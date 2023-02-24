78c5c1b4a9a1b90cc4f17fd6e69a33a69a86b4c4
Argon422 <102897482+Argon422@users.noreply.github.com>
2023-02-24 01:33:38 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
+
+
+/*
+Dom Sammut 2013
+***************
+Web: www.domsammut.com
+Twitter: www.twitter.com/domsammut
+
+View codepen on my website: https://www.domsammut.com/projects/pure-css-loading-animation
+
+*/
